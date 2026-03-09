8f2bb35d6555fc34443916bf082602a7787fabbc
Anindita <chaudhuryanindita27@gmail.com>
2026-03-09 23:11:54 +0530

    added chat model

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose"
 
 const ChatSchema = new mongoose.Schema({
-
-question:String,
-answer:String,
-time:{
-type:Date,
-default:Date.now
-}
-
+  question: String,
+  answer: String,
+  time: {
+    type: Date,
+    default: Date.now
+  }
 })
 
-export default mongoose.model("Chat",ChatSchema)
+export default mongoose.model("Chat", ChatSchema)
